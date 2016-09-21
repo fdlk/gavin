80ffe019a342bdda7782675a8226b1c052248ae5
Mark-de-Haan <markdehaan90@gmail.com>
2016-09-21 16:49:29 +0200

    Remove counter and duck from the app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,18 +4,8 @@ import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName={classes.activeRoute}>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName={classes.activeRoute}>
-      Counter
-    </Link>
-    {' · '}
-    <Link to='/gavin' activeClassName={classes.activeRoute}>
-      Gavin
-    </Link>
+    <h1>MOLGENIS Diagnostics</h1>
+    <Link to='/gavin' activeClassName={classes.activeRoute} />
   </div>
 )
 
