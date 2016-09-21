4e5c18df52efb1fe8dfd5a1571a993e3b2d02b5f
Mark-de-Haan <markdehaan90@gmail.com>
2016-09-21 14:59:13 +0200

    Added Variant Table Container

---
+++ src/routes/Gavin/containers/VariantTableContainer.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+
+// ------------------------------------
+// Presentation components
+// ------------------------------------
+
+const propTypes = {
+  variants : PropTypes.array
+}
+
+class VariantTable extends Component {
+  render () {
+    return (
+        <div>
+            <BootstrapTable ref='table' data={this.props.variants}>
+                <TableHeaderColumn dataField='chromosome' isKey>#CHROM</TableHeaderColumn>
+                <TableHeaderColumn dataField='position'>POS</TableHeaderColumn>
+                <TableHeaderColumn dataField='ref'>REF</TableHeaderColumn>
+                <TableHeaderColumn dataField='alt'>ALT</TableHeaderColumn>
+                <TableHeaderColumn dataField='gene'>GENE</TableHeaderColumn>
+            </BootstrapTable>
+        </div>
+    )
+  }
+}
+
+VariantTable.propTypes = propTypes
+
+// ------------------------------------
+// Container / Presentation wrapping
+// ------------------------------------
+const mapStateToProps = (state) => {
+    return { variants : state.gavin.entities.variants }
+}
+
+const mapDispatchToProps = {}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(VariantTable);
